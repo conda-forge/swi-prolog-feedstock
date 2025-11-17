e02244c28ffe57a45333be15064829c76f5a4e0e
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-11-17 09:08:30 -0600

    try wrapper script

---
+++ recipe/build-sw-prolog-win.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -eux
+
+BUILD_TYPE=Release
+CMAKE_ARGS="${CMAKE_ARGS} -DSWIPL_PACKAGES_X=OFF"
+
+mkdir build
+
+pushd build
+    cmake \
+        -GNinja \
+        ${CMAKE_ARGS:+ ${CMAKE_ARGS}} \
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+        -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+        -DCMAKE_PREFIX_PATH="${PREFIX}" \
+        -DINSTALL_TESTS=ON \
+        -DSWIPL_PACKAGES_QT=OFF \
+        "${SRC_DIR}"
+
+    cmake --build . \
+        -j "${CPU_COUNT}" \
+        --config "${BUILD_TYPE}"
+
+    cmake --build . \
+        -j "${CPU_COUNT}"  \
+        --config "${BUILD_TYPE}" \
+        --target install
+popd
+
+pushd packages/swipy
+    "${PYTHON}" -m pip install . \
+        ${PIP_OPTS:+${PIP_OPTS}}
+popd
+
+pushd packages/mqi/python
+    "${PYTHON}" -m pip install . \
+        ${PIP_OPTS:+${PIP_OPTS}}
+popd
--- recipe/build-swi-prolog.bat
+++ recipe/build-swi-prolog.bat
@@ -1,43 +1,10 @@
 @echo on
 
-set BUILD_TYPE=Release
+set "PREFIX=%PREFIX:\=/%"
+set "SRC_DIR=%SRC_DIR:\=/%"
+set "MSYSTEM=MINGW%ARCH%"
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
 
-set "GMP_INCLUDE_DIRS=%PREFIX%\Library\include"
-set "GMP_LIB_DIRS=%PREFIX%\Library\lib"
-
-set "PIP_OPTS=-vv --no-deps --no-build-isolation"
-
-md build
-
-pushd build
-    cmake %CMAKE_ARGS% ^
-        -GNinja ^
-        -DCMAKE_BUILD_TYPE=Release ^
-        -DINSTALL_TESTS=ON ^
-        "-DCMAKE_INSTALL_PREFIX=%PREFIX%" ^
-        "-DCMAKE_PREFIX_PATH=%PREFIX%" ^
-        "%SRC_DIR%" ^
-        || exit 2
-
-    cmake --build . ^
-        --config "%BUILD_TYPE%" ^
-        || exit 3
-
-    cmake --build . ^
-        --target install ^
-        --config "%BUILD_TYPE%" ^
-        || exit 4
-popd
-
-pushd packages\swipy
-    "%PYTHON%" -m pip install . ^
-        %PIP_OPTS% ^
-        || exit 5
-popd
-
-
-pushd packages\mqi\python
-    "%PYTHON%" -m pip install . ^
-        %PIP_OPTS% ^
-        || exit 6
-popd
+bash -lc "%RECIPE_DIR%/build-sw-prolog-win.sh" ^
+    || exit 2
