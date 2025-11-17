47d1d3f27b780ecb4e31ed3a5813381f5ea60590
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-11-17 09:24:15 -0600

    copy win sh to src_dir

--- recipe/build-swi-prolog.bat
+++ recipe/build-swi-prolog.bat
@@ -1,10 +1,12 @@
 @echo on
 
+copy "%RECIPE_DIR%/build-sw-prolog-win.sh" .
+
 set "PREFIX=%PREFIX:\=/%"
 set "SRC_DIR=%SRC_DIR:\=/%"
 set "MSYSTEM=MINGW%ARCH%"
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 
-bash -lc "%RECIPE_DIR%/build-sw-prolog-win.sh" ^
+bash -lc "build-sw-prolog-win.sh" ^
     || exit 2
